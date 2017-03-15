fd40fbea8a6eec250db958d684c7cee3bcfcd903
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-03-15 08:03:16 -0500

    varchar instead of text

--- dump.sql
+++ dump.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS `post_its`;
 CREATE TABLE `post_its` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `chat_id` bigint(20) NOT NULL,
-  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `text` varchar(210) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
